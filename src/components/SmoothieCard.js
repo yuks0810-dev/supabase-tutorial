a7f6427a0eb6fa9f61827fb87ad96db69a29a184
中田裕貴 <nakatayuki@yukinakata-MacBook-Air.local>
2024-06-15 21:04:08 +0900

    Finish Supabase Tutorial #7 - Deleting Records

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,13 +1,32 @@
 import { Link } from "react-router-dom"
+import supabase from "../config/supabaseClient"
+
 const SmoothieCard = ({smoothie}) => {
+    const handleDelete = async () => {
+        const {data, error} = await supabase
+            .from('smoothies')
+            .delete()
+            .eq('id', smoothie.id)
+
+        if (error) {
+            console.log(error)
+        }
+        if (data) {
+            console.log(data)
+        }
+    }
+
     return (
         <div className="smoothie-card">
             <h3>{smoothie.title}</h3>
             <p>{smoothie.method}</p>
             <div className="rating">{smoothie.rating}</div>
             <div className="buttons">
-                <Link href="href" to={'/' + smoothie.id}>
+                <Link to={'/' + smoothie.id}>
                     <i className="material-icons">edit</i>
+                </Link>
+                <Link to='/'>
+                    <i className="material-icons" onClick={handleDelete}>delete</i>
                 </Link>
             </div>
         </div>
