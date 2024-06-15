0e6a8d5d233f0f35103900f7aa353c3128e8e399
中田裕貴 <nakatayuki@yukinakata-MacBook-Air.local>
2024-06-15 21:13:14 +0900

    Finish Supabase Tutorial #8 - Updating Local State

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,19 +1,23 @@
-import { Link } from "react-router-dom"
 import supabase from "../config/supabaseClient"
+import { Link } from 'react-router-dom'
 
-const SmoothieCard = ({smoothie}) => {
+const SmoothieCard = ({ smoothie, onDelete }) => {
+
     const handleDelete = async () => {
-        const {data, error} = await supabase
+        const { data, error } = await supabase
             .from('smoothies')
             .delete()
             .eq('id', smoothie.id)
 
         if (error) {
             console.log(error)
+            return
         }
         if (data) {
             console.log(data)
+            onDelete(smoothie.id)
         }
+        onDelete(smoothie.id)
     }
 
     return (
@@ -22,12 +26,10 @@ const SmoothieCard = ({smoothie}) => {
             <p>{smoothie.method}</p>
             <div className="rating">{smoothie.rating}</div>
             <div className="buttons">
-                <Link to={'/' + smoothie.id}>
+                <Link to={"/" + smoothie.id}>
                     <i className="material-icons">edit</i>
                 </Link>
-                <Link to='/'>
-                    <i className="material-icons" onClick={handleDelete}>delete</i>
-                </Link>
+                <i className="material-icons" onClick={handleDelete}>delete</i>
             </div>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,12 @@ const Home = () => {
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState(null)
 
+  const handleDelete = (id) => {
+    setSmoothies(prevSmoothies => {
+      return prevSmoothies.filter(sm => sm.id !== id)
+    })
+  }
+
   useEffect(() => {
     const fetchSmoothies = async () => {
       const {data, error} = await supabase
@@ -35,7 +41,11 @@ const Home = () => {
           {/* order-by buttons */}
           <div className="smoothie-grid">
             {smoothies.map(smoothie => (
-              <SmoothieCard key={smoothie.id} smoothie={smoothie}></SmoothieCard>
+              <SmoothieCard
+                key={smoothie.id}
+                smoothie={smoothie}
+                onDelete={handleDelete}
+              />
             ))}
           </div>
         </div>
