e5799c8d0c0bca47871cf22dda6dbc2efe5b7949
中田裕貴 <nakatayuki@yukinakata-MacBook-Air.local>
2024-06-15 20:32:29 +0900

    Finish Supabase Tutorial #5 - Fetching Single Records

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom"
 const SmoothieCard = ({smoothie}) => {
     return (
         <div className="smoothie-card">
             <h3>{smoothie.title}</h3>
             <p>{smoothie.method}</p>
             <div className="rating">{smoothie.rating}</div>
+            <div className="buttons">
+                <Link href="href" to={'/' + smoothie.id}>
+                    <i className="material-icons">edit</i>
+                </Link>
+            </div>
         </div>
     )
 }
